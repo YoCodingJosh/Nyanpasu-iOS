a9de8ac1d590fdc852d4340fde7cc3f00f8a7d61
Josh Kennedy <josh.kennedy@artisantechgroup.com>
2021-01-18 12:17:47 -0600

    Added counter label that increments every button push

--- Nyanpasu/ContentView.swift
+++ Nyanpasu/ContentView.swift
@@ -10,26 +10,30 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
+    
+    @State var counter: Int = 0
 
     var body: some View {
         Button(action: nyanpasu, label: {
             Text("Nyanpasu!")
+        }).onAppear(perform: {
+            // Load data from Core Data
         })
+        
+        Text("You have nyanpasu'd \(self.counter) time\(self.counter == 1 ? "" : "s")")
     }
     
     private func nyanpasu() {
-        playSound()
+        playMySound()
         
         incrementCounter()
-    }
-    
-    private func playSound() {
-        playMySound()
     }
 
     private func incrementCounter() {
         // TODO: Persist count to Core Data
         print("nyanpasu!")
+        
+        self.counter += 1
     }
 }
 
